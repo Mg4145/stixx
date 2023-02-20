5820f6425c23eead31dacf90471e8ff5fbb0b900
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2023-02-19 22:11:07 -0500

    Updated players class with methods

--- src/stixx/players.py
+++ src/stixx/players.py
@@ -8,14 +8,19 @@ class Player:
         self.left = 1
         self.right = 1
 
-    # def __str__(self) -> str:
-    #     """Return the player's name"""
-    #     return self.name + " " + self.current_hand()
-
     def is_both_empty(self) -> bool:
         """Check if either player has no sticks left"""
-        return self.left == 0 or self.right == 0
+        return self.left == 0 and self.right == 0
 
     def current_hand(self) -> str:
         """Return the current hand"""
-        return f"L: {self.left}\nR: {self.right}"
+        return f"{self.left} {self.right}"
+
+    def update(self, hand: str, value: int) -> None:
+        """Update the player's hand"""
+        if hand == "L":
+            self.left = (self.left + value) % 5 if (self.left + value) % 5 != 0 else 0
+        else:
+            self.right = (
+                (self.right + value) % 5 if (self.right + value) % 5 != 0 else 0
+            )
