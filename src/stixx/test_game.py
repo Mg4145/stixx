d4ef755116cf0e233b9bc84e317a078e0cb4177a
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2023-03-01 19:54:18 -0500

    Added testing for game

---
+++ src/stixx/test_game.py
@@ -0,0 +1,19 @@
+import unittest
+from players import Player
+from game import Game
+
+
+class TestGame(unittest.TestCase):
+    def tearUp(self):
+        player1 = Player("Bob")
+        player2 = Player("Sue")
+        self.game = Game(player1, player2)
+        self.game.play()
+
+    def test_game_over(self):
+        self.tearUp()
+        player1 = self.game.player1
+        player2 = self.game.player2
+        player2.left = 0
+        player2.right = 0
+        self.assertTrue(self.game.is_over())
