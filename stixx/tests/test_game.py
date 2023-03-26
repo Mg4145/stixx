6a555451ec3f726f7abb672d5b897ec8e75b3aa1
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2023-03-26 00:06:32 -0400

    Working on tests for game.py:

--- stixx/tests/test_game.py
+++ stixx/tests/test_game.py
@@ -53,3 +53,36 @@ class TestGame(unittest.TestCase):
         player1.right = 0
         self.game.get_winner()
         self.assertEqual(self.game.winner, "Sue")
+
+    def test_valid_input(self):
+        self.tearUp()
+        self.assertTrue(self.game.valid_input("L"))
+        self.assertTrue(self.game.valid_input("R"))
+        self.assertFalse(self.game.valid_input("A"))
+        self.assertFalse(self.game.valid_input("B"))
+
+    def test_coin_toss(self):
+        self.tearUp()
+        self.game.coin_toss()
+        self.assertTrue(self.game.current_player.name == "Bob" or self.game.current_player.name == "Sue")
+
+    def test_switch_players(self):
+        self.tearUp()
+        current_player = self.game.current_player
+        other_player = self.game.opponent
+
+        self.game.switch_players()
+        self.assertEqual(self.game.current_player, other_player)
+        self.assertEqual(self.game.opponent, current_player)
+
+
+    # def test_prompt_dialog(self):
+    #     self.tearUp()
+    #     # Assert that two strings are equal
+    #     start = "-" * 80 + f"\n{self.game.current_player.name} goes first!"
+    #     assert_equal = self.game.prompt_dialog("start")
+    #     print(f"START: {start}")
+    #     print(f"ASSERT_EQUAL: {assert_equal}")
+    #     self.assertEqual(start, assert_equal)
+    # self.game.prompt_dialog("turn")
+    # self.game.prompt_dialog("over")
