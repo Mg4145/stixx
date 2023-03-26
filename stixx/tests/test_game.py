a8e65914a047264b0d44f155d50ae4802eb9ca88
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2023-03-26 12:59:13 -0400

    Wrote unit tests for dialog in game

--- stixx/tests/test_game.py
+++ stixx/tests/test_game.py
@@ -1,10 +1,14 @@
+from players import Player
+from game import Game
 import os
 import sys
 import unittest
+import io
+
 
 sys.path.insert(0, os.path.abspath('..'))
-from game import Game
-from players import Player
+
+LINE = "-" * 80
 
 
 class TestGame(unittest.TestCase):
@@ -75,14 +79,48 @@ class TestGame(unittest.TestCase):
         self.assertEqual(self.game.current_player, other_player)
         self.assertEqual(self.game.opponent, current_player)
 
+    def test_prompt_dialog_start(self):
+        self.tearUp()
 
-    # def test_prompt_dialog(self):
-    #     self.tearUp()
-    #     # Assert that two strings are equal
-    #     start = "-" * 80 + f"\n{self.game.current_player.name} goes first!"
-    #     assert_equal = self.game.prompt_dialog("start")
-    #     print(f"START: {start}")
-    #     print(f"ASSERT_EQUAL: {assert_equal}")
-    #     self.assertEqual(start, assert_equal)
-    # self.game.prompt_dialog("turn")
-    # self.game.prompt_dialog("over")
+        # TEST START DIALOG
+        test_dialog = LINE + f"\n{self.game.current_player.name} goes first!"
+
+        output = io.StringIO()
+        sys.stdout = output
+        self.game.prompt_dialog("start")
+        dialog = output.getvalue().strip()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(dialog, test_dialog)
+
+    def test_prompt_dialog_turn(self):
+        self.tearUp()
+        turn_line_1 = f"\n{self.game.current_player.name} it's your turn!"
+        turn_line_2 = f"\nYour current hand is: {self.game.current_player.current_hand()}"
+        turn_line_3 = f"\n{self.game.opponent.name}'s hand is: {self.game.opponent.current_hand()}\n"
+
+        test_dialog = LINE + turn_line_1 + turn_line_2 + turn_line_3
+        output = io.StringIO()
+        sys.stdout = output
+        self.game.prompt_dialog("turn")
+        dialog = output.getvalue().strip() + "\n"
+        sys.stdout = sys.__stdout__
+        self.assertEqual(dialog, test_dialog)
+
+    def test_prompt_dialog_over(self):
+        self.tearUp()
+        self.game.get_winner()
+        LINE.replace("-", "*")
+        print(self.game.winner)
+        over_line_1 = "\nGame over!" + f"\n{self.game.winner} wins!\n"
+        over_line_2 = "\nFinal Hands: "
+        over_line_3 = f"\n{self.game.player1.name}: {self.game.player1.current_hand()}"
+        over_line_4 = f"\n{self.game.player2.name}: {self.game.player2.current_hand()}"
+
+        test_dialog = LINE + over_line_1 + over_line_2 + over_line_3 + over_line_4 + LINE
+
+        output = io.StringIO()
+        sys.stdout = output
+        self.game.prompt_dialog("over")
+        dialog = output.getvalue().strip()
+        sys.stdout = sys.__stdout__
+        self.assertEqual(dialog, test_dialog)
